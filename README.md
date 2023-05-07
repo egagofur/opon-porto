ac800ffd91783ab545498c9acdc945c040a85825
egagofur <egagofurtriwahana@gmail.com>
2023-05-08 01:04:52 +0700

    cinta portfolio website

---
+++ README.md
@@ -0,0 +1,2 @@
+# Portfolio Website - Cinta
+Built with love ❤️
---
+++ src/data/info.ts
@@ -0,0 +1,50 @@
+export const info = {
+  baseUrl: "https://cintarahmaliya.vercel.app",
+  name: "Cinta Rahmaliya",
+  jobDescription: "Web Developer",
+  about: `I am a software engineer living in Malang Regency, Indonesia. I love making things come to life on the internet, be it websites, apps, or anything in between. Skilled in Front-end Development. I have 1 year experience in HTML, CSS, JavaScript, PHP, Bootstrap and Tailwind CSS. Because of this, I have successfully completed several web projects from scratch. However, I am learning continuously and never stop exploring new technologies. So, I also gain knowledge about Laravel with time. I am a hard-working person and a great team player. I am a fast learner and always eager to learn new things. I am a self-motivated person and I love to take challenges.`,
+
+  experience: [
+    {
+      name: "PT GET APLIKASI INDONESIA [Internship]",
+      location: "Cepokomulyo village, Kepanjen district, Malang Regency",
+      startDate: "Jan 2024",
+      endDate: "Jan 2025",
+      description: [
+        "✓ Developing code in accordance with appropriate design and architectural rules",
+        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
+        "✓ Help and training of other colleagues",
+        "✓ Others tasks connected to the nature of the job",
+        "✓ Code Improvements and Refactoring ",
+        "✓ Best Practices and Rules Maintainer",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "SMK Negeri 1 Kepanjen",
+      location: "Kedungpedaringan, Kepanjen district, Malang Regency",
+      startDate: "2022",
+      endDate: "2025",
+      description: [
+        "Software Engineering",
+      ],
+    },
+  ],
+
+  socialMedia: {
+    github: "https://github.com/CintaaaRasyifa",
+    email: "mailto: egagofurtriwahana@gmail.com",
+  },
+
+  projects: [
+    {
+      title: "Landing Page",
+      isFeatured: true,
+      thumbnail: "/assets/images/project-landingpage.png",
+      githubUrl: "https://github.com/egagofur/opon-landingpage",
+      liveUrl: "https://egagofur.github.io/opon-landingpage/",
+    },
+  ],
+};
